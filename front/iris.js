cbbb3d82b68d752c5e7dcf283a9ca656b5b16e19
108-0 <hdy5049@naver.com>
2025-03-17 16:14:06 +0900

    붓꽃 프론트html

--- front/iris.js
+++ front/iris.js
@@ -1,39 +1,31 @@
 function Send(){
+    sl = document.getElementById("sl")
+    sw = document.getElementById("sw")
+    pl = document.getElementById("pl")
+    pw = document.getElementById("pw")
 
-sl = document.getElementById("sl")
-sw = document.getElementById("sw")
-pl = document.getElementById("pl")
-pw = document.getElementById("pw")
+    var data = {
+        "sepal_length": sl.value,
+        "sepal_width": sw.value,
+        "petal_length": pl.value,
+        "petal_width": pw.value
+    }
 
-  var data = {
-    'sepal_length': sl.value,
-    'sepal_width': sw.value,
-    'petal_length': pl.value,
-    'petal_width': pw.value,
-  }
 
-  $.ajax({
-    type: "POST",
-    url: 'http://localhost:8000/predict',
-    headers:{
-        "Accept" : "application/json",
-        "Content-Type": "application/json",
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8000/predict",
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json",
         },
-    data: JSON.stringify(data),
+        data: JSON.stringify(data),
+       }).done(function(response){
+            console.log(response)
+            txtOut.value = response.prediction + " 일 확율: " + response.probability
 
-  }).done(function(response) {
-
-        txtOut.value = response.prediction + "  " + response.probability
-
-        console.log(response)
-
-
-  }).fail(function(error) {
-    alert("!/js/user.js에서 에러발생: " + error.statusText);
-    console.log(error)
-  }).always(function(r){
-    console.log("always" + r)
-  });
-
-
+       }).fail(function(response){
+            alert("fail" + response)
+       }
+       ).always()
 }
